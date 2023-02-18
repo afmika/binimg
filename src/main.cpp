85264496188fd6730c9cffb041f45862930f4e84
Michaël <afmichael73@gmail.com>
2023-02-18 18:32:13 +0300

    setup writer

--- src/main.cpp
+++ src/main.cpp
@@ -85,6 +85,11 @@ public:
         file.close();
     }
 
+    void loadText(std::string text) {
+        n_bytes = text.size();
+        content = (uint8_t *) text.c_str();
+    }
+
     void saveToFile(std::string filename) const {
         std::ofstream file(filename, std::ios::out | std::ios::binary);
         file.write((char *) content, size());
@@ -100,62 +105,115 @@ public:
         in.close();
         return size;
     }
+    
+    inline void allocate(size_t size) {
+        n_bytes = size;
+        content = (uint8_t*) malloc(size);
+    }
 
     ~File() {
         free(content);
     }
 };
 
-void check(const Image& img) {
+void check(const Bytes& img) {
     if (!img.isLoaded()) {
-        std::cerr << "Image not loaded (invalid or corrupted?)";
+        std::cerr << "File not loaded (invalid or corrupted?)";
         exit(1);
     }
 }
 
-// test filter
-void BlackWhite(const Image& img) {
+void writeOffset(uint8_t* byte, uint8_t data, uint8_t offset) {
+    uint8_t temp = *byte;
+    *byte = (*byte & ~0b11) | (data >> offset);
+}
+
+void readOffset(uint8_t* byte, uint8_t* data, uint8_t offset) {
+    *data |= (*byte & 0b11) << offset;
+}
+
+void encode(const Image& img, const File& file) {
     check(img);
-    for (size_t p = 0; p < img.size(); p += 4) {
-        int total = img.content[p + 0] 
-            + img.content[p + 1] 
-            + img.content[p + 2] 
-            + img.content[p + 3];
-        img.content[p + 0] = total / 4;
-        img.content[p + 1] = total / 4;
-        img.content[p + 2] = total / 4;
-        img.content[p + 3] = total / 4;
+    check(file);
+
+    size_t target_size = file.size();
+    // let's do 2 bits per color component
+    // -4 for the header (4 * 8 = 32 bits)
+    size_t header_offset = 4;
+    size_t available_size = img.size() / img.compSize() - header_offset;
+
+    if (target_size > available_size) {
+        std::cerr << "\nImage cannot contain the file (not enough place)";
+        std::cerr << "\n" << target_size << " > " << available_size;
+        exit(1);
+    }
+
+    // header
+    // 32 bits uint, 1 byte per pixel component
+    size_t p = 0;
+    img.content[p++] = target_size >> 24;
+    img.content[p++] = target_size >> 16;
+    img.content[p++] = target_size >> 8;
+    img.content[p++] = target_size >> 0;
+    // body
+    for (size_t i = 0; i < target_size; i++) {
+        writeOffset(img.content + p++, file.content[i], 6);
+        writeOffset(img.content + p++, file.content[i], 4);
+        writeOffset(img.content + p++, file.content[i], 2);
+        writeOffset(img.content + p++, file.content[i], 0);
     }
 }
 
-void encode(const Image& img, const Bytes& file) {
+void decode(const Image& img, File& file) {
     check(img);
-    int n_bytes;
-    for (size_t p = 0; p < img.size(); p += 4) {
-        img.content[p + 0] = 100;
-        img.content[p + 1] = 200;
-        img.content[p + 2] = 100;
-        img.content[p + 3] = rand() % 255;
+    size_t target_size = 0;
+    size_t p = 0;
+    target_size |= (0xFFFFFFFF & img.content[p++]) << 24;
+    target_size |= (0xFFFFFFFF & img.content[p++]) << 16;
+    target_size |= (0xFFFFFFFF & img.content[p++]) << 8;
+    target_size |= (0xFFFFFFFF & img.content[p++]) << 0;
+
+    std::cout << "\nLoading " << target_size << " bytes";
+    file.allocate(target_size);
+    for (size_t i = 0; i < target_size; i++) {
+        // reconstruct the byte
+        file.content[i] = 0;
+        readOffset(img.content + p++, file.content + i, 6);
+        readOffset(img.content + p++, file.content + i, 4);
+        readOffset(img.content + p++, file.content + i, 2);
+        readOffset(img.content + p++, file.content + i, 0);
     }
 }
 
-void decode(const Image& img) {
-    check(img);
-}
 
-int main() {
-    Image test;
-    test.loadFromFile("b.jpg");
-    if (test.isLoaded()) {
-        test.printInfos();
-        BlackWhite(test);
-        test.saveToFile("sample.png");
-    }
-
+void testEncodeDecode() {
+    // encode
+    Image img;
     File file;
-    file.loadFromFile("b.jpg");
-    if (file.isLoaded()) {
-        file.printInfos();
-        file.saveToFile("qq.jpg");
-    }
+    img.loadFromFile("b.jpg");
+    file.loadFromFile("a.jpg");
+    encode(img, file);
+    img.saveToFile("result.png");
+
+    // decode
+    Image img_result;
+    img_result.loadFromFile("result.png");
+    File dest_file;
+    decode(img_result, dest_file);
+    assert(dest_file.isLoaded());
+    dest_file.saveToFile("decoded.png");
+}
+
+
+void help() {
+    std::cout << "binimg encode container input" << std::endl;
+    std::cout << "binimg decode container" << std::endl;
+    std::cout << "Examples: " << std::endl;
+    std::cout << "binimg encode container.png rickroll.mp4" << std::endl;
+    std::cout << "binimg decode encoded.png" << std::endl;
+}
+
+int main(int argc, char** argv) {
+    testEncodeDecode();
+    return 0;
 }
